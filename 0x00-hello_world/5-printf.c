7370e2d1ed818d89670585c1801ac4e29014c911
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-07 11:23:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point where i create The printf Function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
