4da7d175f5993a89adefa26fcc17928906416357
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-12 16:50:35 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - checking chars.
+ *
+ * Return: the addition of a and b.
+ *
+ * @a: 1st int
+ *
+ * @b: 2nd int
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
