7cd80a914a896f61366e3fc9b9b37f325e9dafa7
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-12 12:10:42 -0700

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checking chars.
+ *
+ * Return: 0 if is upper and 1 if is lower
+ *
+ * @c: char to check
+ */
+
+int print_last_digit(int c)
+{
+	int r = c % 10;
+
+	return (r);
+}
