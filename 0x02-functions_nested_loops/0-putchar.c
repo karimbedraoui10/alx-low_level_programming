e406b9cb73e323260f77ba296c4b232a1a599cf4
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-12 09:27:48 -0700

    ./0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include "main.h"
 /**
  * main - entry points
  *
@@ -6,6 +6,12 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+
+	_putchar('\n');
 	return (0);
 }
