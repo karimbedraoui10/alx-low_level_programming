f1477b7245646ecddc7f583d68bba188f7844fbb
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-12 17:23:03 -0700

    ./11-98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_to_98 - print to 98 .
+ *
+ * @n: print from
+ */
+
+void print_to_98(int n)
+{
+	int m;
+
+	if (n > 98)
+	{
+		for (m = n; m >= 98; m-- ){
+			if (m == 98){
+				printf("%d", m);
+			}
+			else
+				printf("%d, ", m);
+
+		}
+	}
+	else if (n < 98)
+	{
+		for (m = n; m <= 98; m++)
+			if (m == 98)
+			{
+				printf("%d", m);
+			}
+			else
+				printf("%d, ", m);
+	}
+	else printf("%d", n);
+
+	printf("\n");
+}
