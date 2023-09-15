959f974468895f1fc294a5b649f1c727c5fbe1e4
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-15 09:02:38 -0700

    multipling

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiple tow parameters
+ *
+ * @c: 1st parameter
+ *
+ * @d: 2nd parameter
+ *
+ * Return: result of multiple
+ */
+
+int mul(int c, int d)
+{
+	int rslt;
+
+	rslt = c * d;
+	return (rslt);
+}
