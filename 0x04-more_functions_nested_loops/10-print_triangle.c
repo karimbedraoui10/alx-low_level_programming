c16fac5fc3634308f0325cf313503bb1bd799dd5
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-15 12:03:57 -0700

    print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,14 +16,14 @@ void print_triangle(int n)
 	}
 	else
 	{
-		for (m = 0; m < n; m++)
+		for (m = 1; m <= n; m++)
 		{
-			for (d = 0; d < n; d++)
+			for (d = 1; d <= n; d++)
 			{
-				if ((m + d) < n)
+				if ((m + d) <= n)
 					_putchar(' ');
 				else
-					_putshar('#');
+					_putchar('#');
 			}
 			_putchar('\n');
 		}
