b31d5346e816681337db678e43de165c5849aea7
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-15 11:59:14 -0700

    print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - print lines
+ *
+ * @n: number of diagonals
+ */
+
+void print_square(int n)
+{
+	int m, d;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (m = 0; m < n; m++)
+		{
+			for (d = 0; d < n; d++)
+			{
+				if ((m + d) < n)
+					_putchar(' ');
+				else
+					_putshar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
