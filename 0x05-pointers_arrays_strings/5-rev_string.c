00636017bf43cc9ae133733914aa69ed00dcf2df
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-18 11:53:48 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - print the str
+ *
+ * @n: parameter that print
+ */
+void rev_string(char *n)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; n[i] != '\0'; i++)
+		len++;
+	for (i = 0; i < count / 2; i++)
+	{
+		char j;
+
+		j = n[i];
+		n[i] = n[len - 1 - i];
+		n[len 1 - i] = j;
+	}
+}
