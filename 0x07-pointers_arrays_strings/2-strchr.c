4ebfa0c39ae863deff5b203da5d84b7a09ac31c9
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-25 09:16:35 -0700

    strchr2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
+	int i = 0;
 
-	return (NULL);
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
