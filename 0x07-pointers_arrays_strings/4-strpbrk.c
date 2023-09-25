b636e831ddf186af289a2fa50d7e7babef40434d
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-25 13:40:51 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,16 +9,16 @@ char *_strpbrk(char *s, char *accept)
 {
 	int itr, jtr;
 	char *p;
-	itr = 0;
 
+	itr = 0;
 	while (s[itr] != '\0')
 	{
 		jtr = 0;
-		while (accept [jtr] != '\0')
+		while (accept[jtr] != '\0')
 		{
-			if (accept [jtr] == s[itr])
+			if (accept[jtr] == s[itr])
 			{
-				p=&s[itr];
+				p = &s[itr];
 				return (p);
 			}
 			jtr++;
