7c63c71cb4e540e3c3c2626414b22a610022b3cf
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-09-28 07:41:09 -0700

    str_lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function count characters
+ * @s: parameter inputs
+ * Return: the length
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+}
