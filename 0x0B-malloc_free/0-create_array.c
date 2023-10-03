fefa369d03f0f73fcad7fed059c25770ec4a65ab
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-10-03 02:32:57 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * create_array - function to print array
+ * @size: the size
+ * @c: input
+ * Return: the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = malloc(size);
+
+	if (size == 0 || size == 0)
+		return (0);
+	while (size--)
+	{
+		ptr[size] = c;
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+this readme for 0x0B-malloc_free project
