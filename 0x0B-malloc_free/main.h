84727449cdb90dc2179c6299d20cdb65ee091c29
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-10-04 09:58:49 -0700

    add 100 and 101 task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -12,6 +12,8 @@ int **alloc_grid (int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 
 #endif
