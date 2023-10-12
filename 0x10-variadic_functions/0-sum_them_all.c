1fb99e94a59bfc9ad5717e9c9e05259cdef00912
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-10-12 11:34:54 -0700

    add all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - function addition
+ * @n: input count
+ * Return: result
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+this readme for 0x10-variadic_functions prjct
