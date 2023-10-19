8b43e9b77acef9b37d2aa27aad847aaed0130a23
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-10-19 09:42:30 -0700

    update 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * add_node_end - adds a node to the end of the list
- @head: address of pointer to head node
+ * @head: address of pointer to head node
  * @str: str field of node
  * Return: size of list
  */
@@ -31,4 +31,4 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 		*head = new_node;
 	return (new_node);
-}
+!}
