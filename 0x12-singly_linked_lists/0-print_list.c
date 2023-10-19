e9d66712a3bd83e50bee3165b40a800ecbac33b0
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-10-19 08:22:49 -0700

    umdate 2 for main0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,23 @@
 #include "lists.h"
 /**
  * _strlen - function to count size of character on string
- * @c: input
+ * @s: input
  * Return: size
  */
 
 int _strlen(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        if (!s)
-        {
-                return (0);
-        }
-        while (*s++)
-        {
-                i++;
-        }
-        return (i);
+	if (!s)
+	{
+		return (0);
+	}
+	while (*s++)
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
