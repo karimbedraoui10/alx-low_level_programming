196ae8084b0c2acd72b1eede9551be61d914dde1
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-10-24 11:51:05 -0700

    add update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,6 +10,8 @@ size_t print_listint(const listint_t *h)
 	size_t i = 0;
 	const listint_t *current = h;
 
+	if (current == NULL)
+		return (0);
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
