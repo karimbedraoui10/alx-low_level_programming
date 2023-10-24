78f6501f41c23b555c4eafe04aabef635f33e8ff
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-10-24 11:47:43 -0700

    add 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - function to print a content of list
+ * @h: input lists_t
+ * Return: return size of list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		i++;
+	}
+
+	return (i);
+}
