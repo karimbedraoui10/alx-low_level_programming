d10351fd339706f0ae39f929650e1360c7462691
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-11-01 04:12:35 -0700

    add creat file up3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,43 +1,28 @@
 #include "main.h"
 /**
- * _strlen - function to count size of character on string
- * @s: input
- * Return: size
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-	{
-		return (0);
-	}
-	while (*s++)
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * create_file - creates a file
+ * create_file - function to create a file and a content
  * @filename: name of file to create
  * @text_content: text to write
- * Return: 1 on success 0 on failure
+ * Return: 1 on success -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int bytes = 0, len = _strlen(text_content);
+	int file_d;
+	int rwr;
+	int n_letter;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_d == -1)
 		return (-1);
-	if (len)
-		bytes = write(fd, text_content, len);
-	close(fd);
-	return (bytes = len ? 1 : -1);
+	if (!text_content)
+		text_content = "";
+	for (n_letter = 0; text_content[n_letter]; n_letter++)
+		;
+	rwr = write(file_d, text_content, n_letter);
+	if (rwr == -1)
+		return (-1);
+	close(file_d);
+	return (1);
 }
