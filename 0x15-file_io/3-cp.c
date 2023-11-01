8a357459d5e6b949f00f44ad258dac6360e13e71
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-11-01 13:40:17 -0700

    up

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,15 @@
  */
 int main(int argc, char *argv[])
 {
+	int src, dest, n_read = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buf[1024];
 
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	src = open(argv[1], O_RDONLY);
 	return (0);
 }
